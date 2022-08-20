f67a7bb910066af95adb73dfcc5c77e41ed6a016
LENOVO <LENOVO@DESKTOP-JJRNDTI>
2022-08-20 21:09:31 +0530

    Add project files.

---
+++ ps20/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ps20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "moulali";
+            string str2 = "moulali";
+            Console.WriteLine(str1 == str2);
+            Console.Read();
+        }
+    }
+}
